fae9709c4626612443c28a6f837c21fd703b63d8
Alexander Opitz <opitz@pluspol.info>
2017-11-14 17:41:52 +0100

    [RELEASE] Release version 0.2.0 with updated compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'TemplaVoila! Plus for CS SEO',
     'description' => 'Integration of [clickstorm] SEO into TemplaVoilà! Plus.',
     'category' => 'misc',
-    'version' => '0.1.0',
+    'version' => '0.2.0',
     'state' => 'stable',
     'uploadfolder' => 0,
     'clearCacheOnLoad' => 0,
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = [
             'php' => '5.5.0-7.2.99',
             'typo3' => '7.6.0-8.7.99',
             'templavoilaplus' => '7.1.3-7.99.99',
-            'cs_seo' => '2.1.0-2.1.99',
+            'cs_seo' => '2.1.0-2.2.99',
         ],
     ],
     'autoload' => [
