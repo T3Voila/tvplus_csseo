f4d4aea14ebb9168cd0063da0f5c6b62a36aac6d
Alexander Opitz <opitz@pluspol.info>
2018-08-23 12:42:26 +0200

    [TASK] Mark as compatible with CS SEO 3.x, PHP 7.3, TYPO3 9.3.x

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'TemplaVoilà! Plus for CS SEO',
     'description' => 'Integration of [clickstorm] SEO into TemplaVoilà! Plus.',
     'category' => 'misc',
-    'version' => '0.2.1',
+    'version' => '0.2.2',
     'state' => 'stable',
     'uploadfolder' => 0,
     'clearCacheOnLoad' => 0,
@@ -12,10 +12,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'PLUSPOL interactive',
     'constraints' => [
         'depends' => [
-            'php' => '5.5.0-7.2.99',
-            'typo3' => '7.6.0-9.2.99',
+            'php' => '5.5.0-7.3.99',
+            'typo3' => '7.6.0-9.3.99',
             'templavoilaplus' => '7.1.3-7.99.99',
-            'cs_seo' => '2.1.0-2.99.99',
+            'cs_seo' => '2.1.0-3.99.99',
         ],
     ],
     'autoload' => [
