7c424472111c9e8b7cc28fc3fcadf94cb76d2a2f
Alexander Opitz <opitz.alexander@googlemail.com>
2022-06-30 15:16:50 +0200

    [TASK] Update version / namespace

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,24 +3,18 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'TemplaVoilà! Plus for CS SEO',
     'description' => 'Integration of TYPO3 [clickstorm] SEO into TemplaVoilà! Plus.',
     'category' => 'misc',
-    'version' => '0.2.3',
+    'version' => '1.0.0',
     'state' => 'stable',
-    'uploadfolder' => 0,
     'clearCacheOnLoad' => 0,
     'author' => 'Alexander Opitz',
-    'author_email' => 'opitz@pluspol-interactive.de',
-    'author_company' => 'PLUSPOL interactive GbR',
+    'author_email' => 'opitz.alexander@googlemail.com',
+    'author_company' => 'T3Voila Team',
     'constraints' => [
         'depends' => [
-            'php' => '5.5.0-7.3.99',
-            'typo3' => '7.6.0-9.5.99',
-            'templavoilaplus' => '7.1.3-7.99.99',
-            'cs_seo' => '2.1.0-4.99.99',
+            'php' => '7.2.0-8.1.99',
+            'typo3' => '8.7.0-11.5.99',
+            'templavoilaplus' => '8.1.0-8.2.99',
+            'cs_seo' => '6.0.0-7.99.99',
         ],
-    ],
-    'autoload' => [
-        'psr-4' => [
-            'Ppi\\PpiTemplavoilaplusCsseo\\' => 'Classes/',
-        ],
-    ],
+    ]
 ];
